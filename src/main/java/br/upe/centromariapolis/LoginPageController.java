9c7cfbb5105e7c995dc26b5d0e3626ed03dc3cb3
igormath <igorxt9@gmail.com>
2023-11-24 21:38:38 -0300

    Modifica o LoginPageController com a verificação

--- src/main/java/br/upe/centromariapolis/LoginPageController.java
+++ src/main/java/br/upe/centromariapolis/LoginPageController.java
@@ -6,24 +6,52 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class LoginPageController {
 
     @FXML
+    private Button bttContinue;
+    @FXML
+    private MaskedTextField cpf;
+//    @FXML
+//    private TextField cpf;
+    @FXML
+    private Label msgError;
+    @FXML
+    private PasswordField pass;
+
+    @FXML
     public void handleLoginAction(ActionEvent event) {
-        try{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainPage.fxml"));
-        Parent root = loader.load();
+        String cpfText = cpf.getText();
+        String passText = pass.getText();
 
-        Scene scene = new Scene(root);
+        String cpfLimpo = cpfText.replaceAll("[^0-9]", "");
 
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        System.out.println(ValidaCpf.isCPF(cpfLimpo));
+        System.out.println(passText.length());
 
-        stage.setScene(scene);
-        stage.show();
-        } catch (Exception e){
-            e.printStackTrace();
+        if ( ValidaCpf.isCPF(cpfLimpo) && (passText.length() >= 6)){
+            try{
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("MainPage.fxml"));
+
+                Parent root = loader.load();
+
+                Scene scene = new Scene(root);
+
+                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+                stage.setScene(scene);
+                stage.show();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        } else{
+            msgError.setText("Por favor, preencha corretamente\n todos os campos");
         }
     }
 
