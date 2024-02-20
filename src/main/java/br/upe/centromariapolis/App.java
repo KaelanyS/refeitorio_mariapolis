c7fa12d08437e9cc9c624af37428d94b2ae91ae6
igormath <igorxt9@gmail.com>
2024-02-19 22:04:42 -0300

    Atualiza Main class

--- src/main/java/br/upe/centromariapolis/App.java
+++ src/main/java/br/upe/centromariapolis/App.java
@@ -1,5 +1,7 @@
 package br.upe.centromariapolis;
 
+import br.upe.centromariapolis.repository.IRepoCustomer;
+import br.upe.centromariapolis.repository.RepoCustomer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,11 +10,15 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class App extends Application {
+
+    IRepoCustomer repoCustomer = new RepoCustomer();
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("LoginPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1024, 768);
         stage.setTitle("Centro Mariapolis Santa Maria");
+        stage.setUserData(repoCustomer);
         stage.setScene(scene);
         stage.show();
     }
