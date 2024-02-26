5e0733e582cc29e3420206b6e46fae2bc155d64a
igormath <igorxt9@gmail.com>
2024-02-26 11:19:04 -0300

    Atualiza module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module br.upe.centromariapolis {
     requires javafx.fxml;
     requires javafx.graphics;
     requires transitive javafx.base;
+    requires java.persistence;
 
     opens br.upe.centromariapolis to javafx.fxml;
     exports br.upe.centromariapolis;
